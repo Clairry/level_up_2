8b7812492c88e2f338402a5f8e901a5cabbaed26
Svetlana <svetaa-05@yandex.ru>
2018-03-12 14:22:30 +0300

    lesson1 homework. Separate method setField.

--- src/lesson1/ReflectionWork.java
+++ src/lesson1/ReflectionWork.java
@@ -7,10 +7,8 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class ReflectionWork {
-    static ArrayList<Car> carList;
-
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-        carList = new ArrayList<Car>();
+        ArrayList<Car> carList = new ArrayList<Car>();
         String carsLine = null;
         Integer i=0;
 
